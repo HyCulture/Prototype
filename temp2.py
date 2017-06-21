debe3efdf9d8dcc4ad8358c07b7f75989e5b08f7
HyCulture <hyculture.sas@gmail.com>
2017-06-21 15:49:28 +0200

    Create temp2.py

---
+++ temp2.py
@@ -0,0 +1,17 @@
+import time
+import datetime
+
+while True :
+	date = datetime.datetime.now().strftime('%Y-%m-%d %H:%M:%S')
+	tfile = open("/sys/bus/w1/devices/28-80000003962a/w1_slave")
+	text = tfile.read()
+	tfile.close()
+	secondline = text.split("\n")[1]
+	temperaturedata = secondline.split(" ")[9]
+	temperature = float(temperaturedata[2:])
+	temperature = temperature / 1000
+	fichier = open("Temperatures.txt", "a")
+	fichier.write(str(date)+"   ")
+	fichier.write(str(temperature)+'\r\n')
+	fichier.close()
+	time.sleep(5)
