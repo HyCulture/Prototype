7b788b0738f912bbc16e78107d271e959901618b
Vincent Felisat <vincent.felisat@outlook.fr>
2017-07-02 18:11:09 +0200

    Update
    Push new code for temperature sensor

---
+++ Temp_sensor1.py
@@ -0,0 +1,44 @@
+#Importing libraries
+import time, datetime, os, csv
+
+#define date
+date = datetime.datetime.now().strftime('%Y-%m-%d %H:%M:%S')
+tfile = open("/sys/bus/w1/devices/28-80000003962a/w1_slave")
+text = tfile.read()
+tfile.close()
+
+#Write in CSV file if the temperature sensor is active
+if tfile is True:
+    read_seconde_line = text.split("\n")[1] #Skip the first line
+    tempData =  read_seconde_line.split(" ")[9] #Read only temp data
+    temperature = float(tempData[2:]) #Get data
+    temperature = temperature / 1000 #Transform data in degres temprature
+else :
+    return False
+
+
+header = [
+     u'Date',
+     u'Time',
+     u'Data_temp',
+]
+
+values = [
+     [u'date', u'date', u'temperature']
+]
+
+def write_csv(header, values):
+    """
+    Write all data in CSV files
+    """
+    files = open('Data_temperature.csv', 'w')
+    ligneEntete = ";".join(entetes) + "\n"
+    files.write(ligneEntete)
+    for valeur in valeurs:
+        ligne = ";".join(valeur) + "\n"
+        files.write(ligne)
+
+    files.close()
+    time.sleep(5)
+
+write_csv(header, values)
