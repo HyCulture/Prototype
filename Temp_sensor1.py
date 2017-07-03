f5c73a6c18e3a20422ea6fe0e20ee163b803c63e
Vincent Felisat <vincent.felisat@outlook.fr>
2017-07-03 21:25:37 +0200

    Update
    Update

--- Temp_sensor1.py
+++ Temp_sensor1.py
@@ -1,45 +1,60 @@
 #Importing libraries
-import time, datetime, os, csv
+import time, glob, datetime, os, csv
 
-#define date
-date = datetime.datetime.now().strftime('%Y-%m-%d %H:%M:%S')
-tfile = open("/sys/bus/w1/devices/28-80000003962a/w1_slave")
-text = tfile.read()
-tfile.close()
+os.system('modprobe w1-gpio')
+os.system('modprobe w1-therm')
 
-#Convert data temp in degres
-def convert_temp(file):
-    temperature = 0
-    if file is True:
-        read_seconde_line = text.split("\n")[1] #Skip the first line
-        tempData =  read_seconde_line.split(" ")[9] #Read only temp data
-        temperature = float(tempData[2:]) #Get data
-        temperature = temperature / 1000 #Transform data in degres temprature
+while True:
+    try:
+        #define date
+        date = datetime.datetime.now().strftime('%Y-%m-%d %H:%M:%S')
+        tfile = open("/sys/bus/w1/devices/28-80000003962a/w1_slave")
+        text = tfile.read()
+        tfile.close()
 
-convert_temp(tfile)
+        #Convert data temp in degres
+        def convert_temp(file):
+            temperature = 0
+            if file is True:
+                read_seconde_line = text.split("\n")[1] #Skip the first line
+                tempData =  read_seconde_line.split(" ")[9] #Read only temp data
+                temperature = float(tempData[2:]) #Get data
+                temperature = temperature / 1000 #Transform data in degres temprature
 
-#Write in CSV file the temperature sensor
-header = [
-     [u'Date',
-     u'Time',
-     u'Data_temp']
-]
+        convert_temp(tfile)
 
-values = []
-values.append(date[0:10])
-values.append(date[10:20])
-values.append(float(temperature))
+        #Write in CSV file the temperature sensor
+        header = [
+             [u'Date',
+             u'Time',
+             u'Data_temp']
+        ]
 
-def write_csv(header, values):
-    """
-    Write all data in CSV files
-    """
-    with open('Data_temperature.csv', 'w') as files:
-        writer = csv.writer((files), delimiter=',')
-        writer.writerows(header)
-        writer.writerow(values)
+        values = []
+        values.append(date[0:10])
+        values.append(date[10:20])
+        values.append(float(temperature))
 
-    files.close()
-    time.sleep(5)
+        def write_csv(header, values):
+            """
+            Write all data in CSV files
+            """
+            with open('Data_temperature.csv', 'w') as files:
+                writer = csv.writer((files), delimiter=',')
+                writer.writerows(header)
+                writer.writerow(values)
 
-write_csv(header, values)
+            files.close()
+            time.sleep(5)
+
+        write_csv(header, values)
+
+        #If temperature it's get with success or not :
+        if temprature is not None:
+            print('Temp={0:0.1f}* '.format(temperature))
+        else:
+            print('Failed to get reading. Try again!')
+            sys.exit(1)
+
+    except KeyboardInterrupt:
+        quit()
